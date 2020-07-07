63bf8797904120f24c8473d1f958bbd847d7bd90
RAMON SOUZA <ramon.souza1988@gmail.com>
2020-07-07 18:21:17 -0300

    criacao projeto

---
+++ SeleniumTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4007af63-b90f-4a1d-b687-99a1e0cfbe85")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumTest/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest
+{
+    
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        private readonly string chromedriverPath = @"C:\Users\1680395\source\repos\SeleniumTest\SeleniumTest\bin\Debug";
+
+        [Test, Description("Teste da busca do google"), Category("Smoke-Test")]
+        public void MyFirstTest()
+        {
+            Console.WriteLine("Estou aqui!");
+            driver = new ChromeDriver(chromedriverPath);
+            driver.Navigate().GoToUrl("https://www.google.com.br");
+
+            IWebElement txtFind = driver.FindElement(By.Name("q"));
+            txtFind.SendKeys("Selenium Tuturial");
+            txtFind.Submit();
+
+            IWebElement linkText = driver.FindElement(By.XPath("//*[@id='vn1s0p1c0']/h3"));
+            
+            Assert.IsTrue(linkText.Text.Contains("Selenium"));
+
+            driver.Quit();
+
+        }
+
+        [Test, Description("Teste da busca do google"), Category("Priority-Test")]
+        public void MySecondTest()
+        {
+            Console.WriteLine("Estou aqui!");
+            driver = new ChromeDriver(chromedriverPath);
+            driver.Navigate().GoToUrl("https://www.google.com.br");
+
+            IWebElement txtFind = driver.FindElement(By.Name("q"));
+            txtFind.SendKeys("Selenium Tuturial");
+            txtFind.Submit();
+
+            IWebElement linkText = driver.FindElement(By.XPath("//*[@id='vn1s0p1c0']/h3"));
+
+            Assert.IsTrue(linkText.Text.Contains("Selenium"));
+
+            driver.Quit();
+
+
+        }
+    }
+}
